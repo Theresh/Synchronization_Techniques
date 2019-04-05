29181e09e7c350fca5370a5dff1e01f4de23e95d
Theresh <ee17mtech11014@iith.ac.in>
2019-04-05 13:49:22 +0530

    Add existing Files

--- figs/Error_vs_snr.py
+++ figs/Error_vs_snr.py
@@ -40,7 +40,7 @@ x=np.random.randint(0,2,N)
 mx=np.subtract(np.multiply(x,2),1)
 #print mx.shape
 qpsk = qpsk1(N,mx)
-lp = 36
+lp = 18
 es = 1
 Nsnr = 10
 sigma=np.sqrt(0.5*(10.0**(-1/10.0)))
@@ -77,11 +77,11 @@ for t in range (10,10+Nsnr):
 phi_m = np.zeros(out.shape)
 phi_m[0:] = phi
 theta1 = (phi_m-out)**2
-x1 = np.linspace(0,theta1.shape[0],theta1.shape[0])
+x1 = np.linspace(0,theta1.shape[0]-1,theta1.shape[0])
 plt.semilogy(x1,theta1)
 plt.xlabel('$\\frac{Eb}{N0}$(dB)')
 plt.ylabel('$Error$')
 plt.legend(['P=%d'%lp],loc='best')
+plt.grid(True)
 plt.savefig("./Phase_error_with_respect_to_SNR_fixed_pilot.eps")
-plt.grid()
 plt.show()
